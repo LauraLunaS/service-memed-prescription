9a7b7bf396c7ab15ce12010a1c71befd71c08e3b
Laura Luna <lauraluna.siqueira@hotmail.com>
2023-11-06 13:00:08 -0300

    refatorando o home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -7,7 +7,7 @@ const PrescriptionPage: React.FC = () => {
     script.src =
       'https://integrations.memed.com.br/modulos/plataforma.sinapse-prescricao/build/sinapse-prescricao.min.js';
     script.dataset.token =
-      'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.WzM2MzE3LCI2MTA0NGVkZThiMDg4YzdmMmIwMDlkNWM3NmJiMzJjMiIsIjIwMjItMTItMTciLCJzaW5hcHNlLnByZXNjcmljYW8iLCJwYXJ0bmVyLjMuMzE2NDkiXQ.Kv-VSTmXqCI-q6GPiPHF7Q8Prhz2RKy0sL0BWYfoM2I'; 
+      'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.WzM2MzE3LCI2MTA0NGVkZThiMDg4YzdmMmIwMDlkNWM3NmJiMzJjMiIsIjIwMjItMTItMTciLCJzaW5hcHNlLnByZXNjcmljYW8iLCJwYXJ0bmVyLjMuMzE2NDkiXQ.Kv-VSTmXqCI-q6GPiPHF7Q8Prhz2RKy0sL0BWYfoM2I';
 
     script.addEventListener('load', () => {
     });
@@ -28,3 +28,4 @@ const PrescriptionPage: React.FC = () => {
 };
 
 export default PrescriptionPage;
+
