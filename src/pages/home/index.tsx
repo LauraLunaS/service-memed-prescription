09cfe8baafa431d04be6693f6c053d7f6572717f
danilolima17 <danilodavite20@hotmail.com>
2023-11-06 15:02:18 -0300

    feat: adicionando modulos

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,21 +2,33 @@ import React, { useEffect } from 'react';
 
 const PrescriptionPage: React.FC = () => {
   useEffect(() => {
-    const script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src =
-      'https://integrations.memed.com.br/modulos/plataforma.sinapse-prescricao/build/sinapse-prescricao.min.js';
-    script.dataset.token =
-      'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.WzM2MzE3LCI2MTA0NGVkZThiMDg4YzdmMmIwMDlkNWM3NmJiMzJjMiIsIjIwMjItMTItMTciLCJzaW5hcHNlLnByZXNjcmljYW8iLCJwYXJ0bmVyLjMuMzE2NDkiXQ.Kv-VSTmXqCI-q6GPiPHF7Q8Prhz2RKy0sL0BWYfoM2I';
+    function initMemedScript() {
+      const script = document.createElement("script");
+      script.src = "https://integrations.memed.com.br/modulos/plataforma.sinapse-prescricao/build/sinapse-prescricao.min.js";
+      script.dataset.token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.WzM2MzE3LCI2MTA0NGVkZThiMDg4YzdmMmIwMDlkNWM3NmJiMzJjMiIsIjIwMjItMTItMTciLCJzaW5hcHNlLnByZXNjcmljYW8iLCJwYXJ0bmVyLjMuMzE2NDkiXQ.Kv-VSTmXqCI-q6GPiPHF7Q8Prhz2RKy0sL0BWYfoM2I";
+      script.addEventListener("load",prescriptionMemed);
+      document.body.appendChild(script);
+    }
 
-    script.addEventListener('load', () => {
-    });
+    function prescriptionMemed() {
+      
+      if (window.MdSinapsePrescricao && window.MdHub) {
+        window.MdSinapsePrescricao.event.add("core:moduleInit", async function (module: any) {
+          if (module.name === "plataforma.prescricao") {
+            await window.MdHub.command.send("plataforma.prescricao", "setPaciente", {
+              idExterno: "algum_tipo_de_hash_ou_id",
+              nome: "Jhon D. Rockefeller",
+              telefone: "99999999999",
+              cpf: "99999999999",
+           
+            });
+            window.MdHub.module.show("plataforma.prescricao");
+          }
+        });
+      }
+    }
 
-    document.body.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script);
-    };
+    initMemedScript();
   }, []);
 
   return (
