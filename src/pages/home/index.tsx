98d5c5fe671e53a6d567cf02ff2df7c88e2d1fde
Laura Luna <lauraluna.siqueira@hotmail.com>
2023-11-07 10:37:30 -0300

    refactor: :art: Refatorando estilos

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -6,6 +6,7 @@ const Home: React.FC = () => {
       const script = document.createElement('script');
       script.src = 'https://integrations.memed.com.br/modulos/plataforma.sinapse-prescricao/build/sinapse-prescricao.min.js';
       script.dataset.token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.WzM2MzE3LCI2MTA0NGVkZThiMDg4YzdmMmIwMDlkNWM3NmJiMzJjMiIsIjIwMjItMTItMTciLCJzaW5hcHNlLnByZXNjcmljYW8iLCJwYXJ0bmVyLjMuMzE2NDkiXQ.Kv-VSTmXqCI-q6GPiPHF7Q8Prhz2RKy0sL0BWYfoM2I';
+      script.setAttribute('data-color', "#4A90A7");
       script.addEventListener('load', prescricaoMemed);
       document.body.appendChild(script);
     }
@@ -15,10 +16,10 @@ const Home: React.FC = () => {
         if (module.name === 'plataforma.prescricao') {
           await (window as any).MdHub.command.send('plataforma.prescricao', 'setPaciente', {
             idExterno: 'algum_tipo_de_hash_ou_id', 
-            nome: 'Jhon D. Rockefeller', 
-            telefone: '99999999999', 
-            cpf: '99999999999', 
-            nome_social: 'Ms Johana Doe', 
+            nome: 'Roberto Silva', 
+            telefone: '81 995678405', 
+            cpf: '000.000.000-03', 
+            nome_social: 'Roberto Silva', 
             endereco: 'Rua da Saúde, 123', 
           });
           (window as any).MdHub.module.show('plataforma.prescricao');
